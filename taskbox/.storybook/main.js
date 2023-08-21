b3756c2d278e648ab2ecb3e234b0f2b39488c559
muhammed-gumus <mr.silver.mg@gmail.com>
2023-08-22 00:52:57 +0300

    (taskbox): installed packages and configured for accessibility

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -2,18 +2,19 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/components/**/*.stories.@(js|jsx)'],
-   staticDirs: ['../public'],
-   addons: [
-     '@storybook/addon-links',
-     '@storybook/addon-essentials',
-     '@storybook/addon-interactions',
-   ],
-   framework: {
-     name: '@storybook/react-vite',
-     options: {},
-   },
-   docs: {
-     autodocs: 'tag',
-   },
- };
- export default config;
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+   '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
