ce3c32b80f73445dc5eb72125c8bb11381c98829
muhammed-gumus <mr.silver.mg@gmail.com>
2023-08-22 00:26:26 +0300

    feat(taskbox): created the first component and it's stories

---
+++ taskbox/src/components/Task.stories.jsx
@@ -0,0 +1,36 @@
+
+import Task from './Task';
+
+export default {
+  component: Task, //the component itself
+  title: 'Task', //how to group or categorize the component in the Storybook sidebar
+  tags: ['autodocs'], //to automatically generate documentation for our components
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX',
+    },
+  },
+};
+
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
