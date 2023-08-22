b7021d9060387c78e1f24c8b67d0d80045dd387a
muhammed-gumus <mr.silver.mg@gmail.com>
2023-08-23 00:43:36 +0300

    feat(taskbox): updated preview.js and InboxStories.stories.js for capture network requests

--- taskbox/.storybook/preview.js
+++ taskbox/.storybook/preview.js
@@ -1,9 +1,15 @@
+import "../src/index.css";
 
-import '../src/index.css';
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
 
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
